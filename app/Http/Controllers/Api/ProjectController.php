076ae096dfd797a7f643bf759b2f59886eb21ebc
marcodedemo <marco.dedemo@gmail.com>
2023-05-31 16:56:06 +0200

    feat: index api request with type filter

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,20 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
 
-        $projects = Project::with('technologies', 'type')
-        ->orderBy('projects.created_at', 'desc')
-        ->paginate(8);
 
+        $requestData = $request->all();
+        
+        $types = Type::all();
+
+        if($request->has('type_id') && $requestData['type_id']) {
+
+            $projects = Project::where('type_id', $requestData['type_id'])
+                ->with('technologies', 'type')
+                ->orderBy('projects.created_at', 'desc')
+                ->get();
+
+                if(count($projects) == 0){
+
+                    return response()->json([
+
+                        'success' => false,
+                        'error' => 'Non ci sono progetti appartenenti a questo type',
+                    ]);
+                }
+        }else{
+
+            $projects = Project::with('technologies', 'type')
+            ->orderBy('projects.created_at', 'desc')
+            ->paginate(8);
+    
+        }
+    
         return response()->json([
             'success' => true,
             'results' => $projects,
+            'types' => $types,
         ]);
+
     }
 
 
